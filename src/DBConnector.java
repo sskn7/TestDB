1a109e8ade0819c58ba757236036996467523e34
kana sasazawa <kana.yellow7@gmail.com>
2025-05-14 01:10:49 +0900

    fix: :sparkles: #2 エラー解消、コンソール表示

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -1,4 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnector {
+	private static String driverName = "com.mysql.jdbc.Driver";
 
+	private static String url =
+			"jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+
+	private static String user = "root";
+	private static String password = "";
+
+	public Connection getConnection() {
+		Connection con = null;
+
+	//↓例外処理の構文
+	try {
+		Class.forName(driverName);
+		//	↓ドライバーがロードされてツ使えるようにな状態にする
+		con = DriverManager.getConnection(url,user,password);
+	//↓エラーが発生したら受け取る
+	} catch (ClassNotFoundException e) {
+	e.printStackTrace();
+	} catch (SQLException e) {
+	e.printStackTrace();
+	}
+	return con;
+	}
 }
